55d5fee5411ab98068839cab94a76b92e0a249d9
davis8988 <Davismarsel@gmail.com>
2019-12-23 23:39:04 +0200

    Added powershell dir to gitignore & Added grafanaApi() function

--- powershell/ExportGrafanaDashboards.ps1
+++ powershell/ExportGrafanaDashboards.ps1
@@ -1,33 +1,37 @@
 ﻿
 
 # Psrerequisites:
-#  Get API Key from Grafana at: Settings -> API Keys (viewer key will suffice)
+#   Powershell v2 or higher
+#   Get API Key from Grafana at: Settings -> API Keys (A viewer-key will suffice only for dashboards and folders. An admin-key will be required for datasources)
 
 # If using powershell v2: $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $scriptPath = $PSScriptRoot
 
 $grafana_home_url = "http://13.90.250.12:3000"
-$api_key = "eyJrIjoiS3pDM2QyT1dYZ1Q3V0kxV1hqcW9aMXY4cEpUNWxueVkiLCJuIjoiZGFzaGJvYXJkc19leHBvcnRfaW1wb3J0IiwiaWQiOjF9"
-
-# Do not change unless you know what you are doing:
-$grafana_api_error_msg = "If you're seeing this Grafana has failed to load its application files"
+$api_key = "eyJrIjoiSzlEUUNvUG9kdGRvRWlFMXh2eWFuZ0VzV1NJNm0xaU0iLCJuIjoiZGFzaGJvYXJkc19leHBvcnRfaW1wb3J0X2FkbWluIiwiaWQiOjF9"
 
 
-function exportDashboards() {
-	$grafana_dashboards_api_url = "${grafana_home_url}/api/search"
-	
-	Write-Host "Exporting dashborads from: $grafana_home_url"
-
-	Write-Host "Querying.."
-	$basicAuth = [string]::Format("Bearer {0}", "${api_key}")
+function grafanaApiCall() {
+	Param(
+		[parameter(Mandatory=$true, Position=0)]
+		[String] $grafanaHost,
+		[parameter(Mandatory=$true, Position=1)]
+		[String] $apiType,
+		[parameter(Mandatory=$true, Position=2)]
+		[String] $apiKey
+    )
+	# Do not change unless you know what you are doing:
+	$grafana_api_error_msg = "If you're seeing this Grafana has failed to load its application files"
+	$basicAuth = [string]::Format("Bearer {0}", "${apiKey}")
 	$headers = @{"Authorization" = $basicAuth }
-
+	
+	$grafanaFullUri = "$grafanaHost/api/$apiType"
 	try {
-		$dashboards = Invoke-RestMethod -Uri $grafana_dashboards_api_url -Headers $headers -ContentType 'application/json'
+		$resultData = Invoke-RestMethod -Uri $grafanaFullUri -Headers $headers -ContentType 'application/json'
 		
 		# Check for errors:
-		if ($dashboards -eq $null -or $dashboards.count -eq $null) {Read-Host "Failed querying grafana at: $grafana_dashboards_api_url using defined 'api_key' in this script"; exit 1}
-		if ($dashboards -Like "*${grafana_api_error_msg}*")        {Read-Host "Failed querying grafana at: $grafana_dashboards_api_url using defined 'api_key' in this script"; exit 1}
+		if (($resultData -eq $null) -or ($resultData -Like "*${grafana_api_error_msg}*")) {Read-Host "Failed querying grafana api at: $grafanaFullUri using defined 'api_key' in this script"; exit 1}
+		return $resultData
 	} catch {
 		# Note that value__ is not a typo.
 		Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
@@ -35,8 +39,18 @@ function exportDashboards() {
 		Read-Host "Exception Message:" $_.Exception.Message
 		exit 1
 	}
+}
 
 
+function exportDashboardsAndFolders() {
+	$grafana_dashboards_api_url = "${grafana_home_url}/api/search"
+	
+	Write-Host "Exporting dashborads from: $grafana_home_url"
+
+	Write-Host "Querying.."
+	
+	$dashboards = grafanaApiCall "$grafana_home_url" "search" "$api_key"
+	
 	Write-Host "Found $($dashboards.count) dashboards"
 
 	Write-Host "Creating exported dashboards and folders dirs next to this script at: $scriptPath"
@@ -47,27 +61,47 @@ function exportDashboards() {
 		Write-Host "Exporting: $($dash.title)"
 		$dash_url = "${grafana_home_url}/api/dashboards/uid/$($dash.uid)"
 		
-		try {
-			$dash_content = Invoke-RestMethod -Uri $dash_url -Headers $headers -ContentType 'application/json'
-		} catch {
-			# Note that value__ is not a typo.
-			Write-Error "Failed to export a dashboard"
-			Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
-			Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
-			Write-Host "Exception Message:" $_.Exception.Message
-			continue
-		}
+		$dash_Content = grafanaApiCall "$grafana_home_url" "dashboards/uid/$($dash.uid)" "$api_key"
 
-		$dash_title = $dash_content.dashboard.title
-		$dash_content.dashboard.id = $null   # When importing always assign $null to the dashboard.id field
-		$dash_content.dashboard.uid = $null  # When importing if setting dashboard.uid field to $null it will create a new dashboard. But will overwrite an existing dashboard otherwise
-		$dash_content.dashboard.version = 1
-		$dash_content.meta.version = 1
+		$dash_title = $dash_Content.dashboard.title
+		$dash_Content.dashboard.id = $null   # When importing always assign $null to the dashboard.id field
+		$dash_Content.dashboard.uid = $null  # When importing if setting dashboard.uid field to $null it will create a new dashboard. But will overwrite an existing dashboard otherwise
+		$dash_Content.dashboard.version = 1  # Reset dashboard version
+		$dash_Content.meta.version = 1
 
 		Write-Host "Saving exported: $dash_title"
-		if ($dash_content.meta.isFolder) {$dash_content | ConvertTo-Json | Out-File "$scriptPath\exported_folders\$dash_title.json"}
-		else {$dash_content | ConvertTo-Json | Out-File "$scriptPath\exported_dashboards\$dash_title.json"}
+		if ($dash_Content.meta.isFolder) {$dash_Content | ConvertTo-Json | Out-File "$scriptPath\exported_folders\$dash_title.json"}
+		else {$dash_Content | ConvertTo-Json | Out-File "$scriptPath\exported_dashboards\$dash_title.json"}
 
+		Write-Host "`n"
+
+	}
+	
+}
+
+function exportDatasources() {
+	
+	Write-Host "Exporting datasources from: $grafana_home_url"
+	Write-Host "Querying.."
+	
+	$datasources = grafanaApiCall "$grafana_home_url" "datasources" "$api_key"
+
+	Write-Host "Found $($datasources.count) datasources"
+
+	Write-Host "Creating exported datasources dir next to this script at: $scriptPath"
+	New-Item -ItemType Directory -Path "$scriptPath\exported_datasources" -Force
+
+	foreach ($dataSrc in $datasources) {
+		Write-Host "Exporting: $($dataSrc.title)"
+		$dataSrc_url = "${grafana_home_url}/api/datasources/$($dataSrc.id)"
+		
+		$dataSrc_Content = grafanaApiCall "$grafana_home_url" "datasources/$($dataSrc.id)" "$api_key"
+
+		$dataSrc_Name = $dataSrc_Content.name
+		$dataSrc_Content.id = $null   # When importing always assign $null to the datasource.id field
+
+		Write-Host "Saving exported: $dataSrc_Name"
+		$dataSrc_Content | ConvertTo-Json | Out-File "$scriptPath\exported_datasources\$dataSrc_Name.json"
 		Write-Host "`n"
 
 	}
@@ -81,8 +115,10 @@ function exportDashboards() {
 
 function main() {
 	Write-Host "Exporter Started"
-	exportDashboards
+	exportDashboardsAndFolders
+	exportDatasources
 	Write-Host "Exporter Finished"
+	Read-Host
 }
 
 
